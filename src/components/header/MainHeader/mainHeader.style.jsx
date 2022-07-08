f8dbf08983271d78b5baf76964271a61240b6162
subincdev <subinc.dev@gmail.com>
2022-07-08 14:37:51 +0900

    feat: 피드 페이지에 사용될 메인 헤더 컴포넌트 추가

---
+++ src/components/header/MainHeader/mainHeader.style.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const BasicHeaderLayout = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: sticky;
+  top: 0;
+  height: 4.8rem;
+  padding: 0 1.2rem 0 1.6rem;
+`;
+
+export const HeaderTitle = styled.h2`
+  font-size: 1.8rem;
+  margin-right: auto;
+`;
+
+export const SearchBtn = styled.button`
+  margin-right: 0.4rem;
+`;
