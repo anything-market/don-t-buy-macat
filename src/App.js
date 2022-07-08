e379c06f1eab322919fb9066236a9b4cf6a5e073
yunheejo <rhcjrdbsgml@gmail.com>
2022-07-08 10:00:03 +0900

    chore: GlobalStyles추가, BrowserRouter추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <GlobalStyles />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
