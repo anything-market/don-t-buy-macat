f51b337e4351c95f582886229ea66c4a0a22605e
subincdev <subinc.dev@gmail.com>
2022-07-10 21:06:51 +0900

    chore: 헤더 컴포넌트 이름 오류 수정

--- src/components/header/MainHeader/MainHeader.jsx
+++ src/components/header/MainHeader/MainHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as S from './mainHeader.style';
 
-export default function ChatHeader() {
+export default function MainHeader() {
   return (
     <S.BasicHeaderLayout>
       <S.HeaderTitle>사지마켓 피드</S.HeaderTitle>
