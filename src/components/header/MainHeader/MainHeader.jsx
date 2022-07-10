1fc768dcddea15c70781709f3d4ff0ec8a531251
subincdev <subinc.dev@gmail.com>
2022-07-10 23:59:11 +0900

    feat: 검색 버튼을 누르면 검색 페이지로 넘어가는 기능 추가

--- src/components/header/MainHeader/MainHeader.jsx
+++ src/components/header/MainHeader/MainHeader.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import * as S from './mainHeader.style';
+import { useNavigate } from 'react-router-dom';
 
 export default function MainHeader() {
+  const navigate = useNavigate();
+  const handleGoSearch = () => {
+    navigate('user/searchuser');
+  };
   return (
     <S.BasicHeaderLayout>
       <S.HeaderTitle>사지마켓 피드</S.HeaderTitle>
-      <S.SearchBtn>
+      <S.SearchBtn onClick={handleGoSearch}>
         <S.SearchIcon />
       </S.SearchBtn>
     </S.BasicHeaderLayout>
