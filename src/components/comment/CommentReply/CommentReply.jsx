fb981747095cc3383396f3cba4e11c72d60e2750
yunheejo <rhcjrdbsgml@gmail.com>
2022-07-11 17:22:07 +0900

    feat: 서버에 데이터 전송 기능 추가
    게시버튼을 누르면 클릭이벤트가 발생하면서 axios로 통신하여 서버에
    데이터를 저장합니다.(임시토큰사용)

--- src/components/comment/CommentReply/CommentReply.jsx
+++ src/components/comment/CommentReply/CommentReply.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { useState } from 'react';
 import {
   CommentReplyWrapper,
@@ -11,6 +12,10 @@ import {
 function CommentReply() {
   const [comment, setComment] = useState('');
   const [isValid, setIsValid] = useState(false);
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyY2I4N2FhODJmZGNjNzEyZjQzODJhNCIsImV4cCI6MTY2MjY4OTcyMSwiaWF0IjoxNjU3NTA1NzIxfQ.eBZXMW7UGe1CCX23sZSf1qtIqnD-lCeIHsySOUdCJCg';
+  const postId = '62cb8b2882fdcc712f4382bb';
+  const commentTxt = '우와 양천향교 폐관수련이다';
 
   const handleInput = (e) => {
     e.preventDefault;
@@ -20,6 +25,29 @@ function CommentReply() {
 
   const changeButton = (e) => {
     e.target.value.length > 0 ? setIsValid(true) : setIsValid(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios({
+      url: `https://mandarin.api.weniv.co.kr/post/${postId}/comments`,
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-type': 'application/json',
+      },
+      data: {
+        comment: {
+          content: `${commentTxt}`,
+        },
+      },
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
   };
 
   return (
@@ -33,7 +61,10 @@ function CommentReply() {
           onChange={handleInput}
           onKeyUp={changeButton}
         />
-        <CommentSubmitButton disabled={isValid ? false : true}>
+        <CommentSubmitButton
+          onClick={handleSubmit}
+          disabled={isValid ? false : true}
+        >
           게시
         </CommentSubmitButton>
       </CommentReplyContainer>
