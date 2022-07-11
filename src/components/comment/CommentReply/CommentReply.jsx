450bc7200ab09ad71ab30ec53b1ca4c25e6341b3
yunheejo <rhcjrdbsgml@gmail.com>
2022-07-11 16:59:13 +0900

    feat: 입력처리 기능
    input에 글을 적으면 콘솔창에 출력값이 나타납니다

--- src/components/comment/CommentReply/CommentReply.jsx
+++ src/components/comment/CommentReply/CommentReply.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState } from 'react';
+
 import {
   CommentReplyWrapper,
   CommentReplyContainer,
@@ -7,16 +9,29 @@ import {
   CommentSubmitButton,
 } from './commentReply.style';
 
-function Comment() {
+function CommentReply() {
+  const [comment, setComment] = useState('');
+
+  const handleInput = (e) => {
+    e.preventDefault;
+    setComment(e.target.value);
+    console.log(e.target.value);
+  };
+
   return (
     <CommentReplyWrapper>
       <CommentReplyContainer>
         <ProfileImg />
-        <CommentInput type="text" placeholder="댓글 입력하기..." />
+        <CommentInput
+          type="text"
+          value={comment}
+          placeholder="댓글 입력하기..."
+          onChange={handleInput}
+        />
         <CommentSubmitButton>게시</CommentSubmitButton>
       </CommentReplyContainer>
     </CommentReplyWrapper>
   );
 }
 
-export default Comment;
+export default CommentReply;
