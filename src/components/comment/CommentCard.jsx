f5847a89149d62771825e5a09423546f25db9307
yunheejo <rhcjrdbsgml@gmail.com>
2022-07-13 16:14:06 +0900

    feat: axios get을 사용하여 하위 컴포넌트에게 데이터 전달
    axios get을 사용하여 댓글 데이터를 불러와 하위 컴포넌트에게 데이터를
    전달했습니다

--- src/components/comment/CommentCard.jsx
+++ src/components/comment/CommentCard.jsx
@@ -1,11 +1,36 @@
+import axios from 'axios';
+import React, { useState } from 'react';
 import CommentList from './CommentList/CommentList';
 import CommentReply from './CommentReply/CommentReply';
 
 function CommentView() {
+  const [comments, setComments] = useState([]);
+
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyY2I4N2FhODJmZGNjNzEyZjQzODJhNCIsImV4cCI6MTY2MjY4OTcyMSwiaWF0IjoxNjU3NTA1NzIxfQ.eBZXMW7UGe1CCX23sZSf1qtIqnD-lCeIHsySOUdCJCg';
+  const postId = '62cb8b2882fdcc712f4382bb';
+  const getComments = () => {
+    axios({
+      url: `https://mandarin.api.weniv.co.kr/post/${postId}/comments?limit=10`,
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-type': 'application/json',
+      },
+    })
+      .then((response) => {
+        console.log(response.data.comments);
+        setComments(response.data.comments);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="CommentView">
-      <CommentList />
-      <CommentReply />
+      <CommentList comments={comments} />
+      <CommentReply getComments={getComments} />
     </div>
   );
 }
