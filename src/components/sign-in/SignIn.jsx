3c83e445faf8729c58518cf2d163dbd46cce1f28
Koreanhamster <jihyun.heo1226@gmail.com>
2022-07-14 14:50:55 +0900

    feat: 회원가입 유효성검사
    - 이메일인풋에서 포커싱 잃을 때 이메일 형식 충족하지 못하거나 비어있으면 메세지 출력 & isValidatedEamil 값 false로 설정
    - 비밀번호인풋에서 포커싱 잃을 때 6자리 미만이면 메세지 출력 & isValidatedPassword 값 false로 출력
    - 다음버튼 눌렀을 때 isValidatedEmail 혹은 isValidatedPassword 둘중에 하나라도 false일시 button disabled

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -4,27 +4,53 @@ import * as S from './signin.style';
 import { useNavigate } from 'react-router-dom';
 
 function SignIn() {
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate();
-  const [message, setMessage] = useState('');
 
-  const nextHandler = function () {
-    navigate('/join/setprofile', {
-      state: {
-        email: email,
-        password: password,
-      },
-    });
+  const [emailMessage, setEamilMessage] = useState('');
+  const [passwordMessage, setpasswordMessage] = useState('');
+
+  const [isValidatedEmail, setIsValidatedEmail] = useState(false);
+  const [isValidatedPassword, setIsValidatedPassword] = useState(false);
+
+  const sendDataToSetProfile = function () {
+    // 서버통신 통과하면 이메일/패스워드 정보 넘겨주기
+    if (isValidatedEmail && isValidatedPassword) {
+      navigate('/join/setprofile', {
+        state: {
+          email: email,
+          password: password,
+        },
+      });
+    } else {
+      console.log('아디비번 제대로치삼');
+    }
+    // 안되면 메세지 출력
   };
 
+  // 이메일 정규식 통과 못할시 메세지 설정, false
   const validateEmail = () => {
     const regexEmail =
       /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
     if (!regexEmail.test(email) && email !== '') {
-      setMessage('*올바른 이메일 형식이 아닙니다.');
+      setIsValidatedEmail(false);
+      setEamilMessage('*올바른 이메일 형식이 아닙니다.');
     } else {
-      setMessage('');
+      setIsValidatedEmail(true);
+      setEamilMessage('');
+    }
+  };
+
+  // 비밀번호 6자리 이상 아닐 시 메세지 설정, false
+  const validatePassword = () => {
+    if (password.length < 6 && password !== '') {
+      setpasswordMessage('*비밀번호는 6자 이상이어야 합니다.');
+      setIsValidatedPassword(false);
+    } else {
+      setpasswordMessage('');
+      setIsValidatedPassword(true);
     }
   };
 
@@ -44,9 +70,9 @@ function SignIn() {
             }}
             onBlur={validateEmail}
           />
-          <p className="message">{message}</p>
+          <p className="message">{emailMessage}</p>
 
-          <label htmlFor="password"></label>
+          <label htmlFor="password">비밀번호</label>
           <input
             type="password"
             id="passWord"
@@ -55,8 +81,17 @@ function SignIn() {
             onChange={(e) => {
               setPassword(e.target.value);
             }}
+            onBlur={validatePassword}
           />
-          <S.Button onClick={nextHandler}>다음</S.Button>
+          <p className="message">{passwordMessage}</p>
+          <S.Button
+            disabled={
+              isValidatedEmail && setIsValidatedEmail ? 'disabled' : null
+            }
+            onClick={sendDataToSetProfile}
+          >
+            다음
+          </S.Button>
         </S.FormBox>
       </form>
     </S.Wrapper>
