224c6bc940836d11f40b4a9ec988fbb946eed01f
subincdev <subinc.dev@gmail.com>
2022-07-14 10:39:14 +0900

    refactor: 기본 헤더 컴포넌트의 버튼을 모달 버튼 컴포넌트로 교체 및 불필요한 코드 삭제

--- src/components/header/BasicHeader/BasicHeader.jsx
+++ src/components/header/BasicHeader/BasicHeader.jsx
@@ -1,27 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Modal from '../../modal/Modal/Modal';
-import LogoutAlert from '../../modal/HeaderModal/LogoutAlert/LogoutAlert';
 import * as S from './basicHeader.style';
+import ModalBtn from '../../modal/ModalBtn/ModalBtn';
 
 export default function BasicHeader() {
   const navigate = useNavigate();
   const handleGoPrev = () => {
     navigate(-1);
-  };
-  //모달열기
-  const [isOpenModal, setIsOpenModal] = useState(false);
-  const handleOpenModal = () => {
-    setIsOpenModal(true);
-  };
-  //alert창 열기
-  const [isOpenAlert, setIsOpenAlert] = useState(false);
-  const handleOpenAlert = () => {
-    setIsOpenAlert(true);
-  };
-
-  const handleCloseAlert = () => {
-    setIsOpenAlert(false);
   };
   return (
     <>
@@ -29,26 +14,8 @@ export default function BasicHeader() {
         <button onClick={handleGoPrev}>
           <S.ArrowLeftIcon />
         </button>
-        <button onClick={handleOpenModal}>
-          <S.MoreIcon />
-        </button>
+        <ModalBtn contents="basic" />
       </S.BasicHeaderLayout>
-      {isOpenModal && (
-        <>
-          <Modal
-            setIsOpenModal={setIsOpenModal}
-            setIsOpenAlert={setIsOpenAlert}
-          >
-            <li>
-              <button>설정 및 개인정보</button>
-            </li>
-            <li>
-              <button onClick={handleOpenAlert}>로그아웃</button>
-            </li>
-          </Modal>
-          {isOpenAlert && <LogoutAlert handleCloseAlert={handleCloseAlert} />}
-        </>
-      )}
     </>
   );
 }
