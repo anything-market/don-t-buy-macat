f1633177896d5f71eee1f47340f81e3eb3bb8259
subincdev <subinc.dev@gmail.com>
2022-07-14 17:11:28 +0900

    refactor: 모달 버튼 컴포넌트 수정으로 기본 모달 콘텐츠를 직접 불러와주는 코드로 수정

--- src/components/header/BasicHeader/BasicHeader.jsx
+++ src/components/header/BasicHeader/BasicHeader.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as S from './basicHeader.style';
 import ModalBtn from '../../modal/ModalBtn/ModalBtn';
+import BasicModalContent from '../../modal/modalContent/BasicModalContent/BasicModalContent';
 
 export default function BasicHeader() {
   const navigate = useNavigate();
@@ -14,7 +15,9 @@ export default function BasicHeader() {
         <button onClick={handleGoPrev}>
           <S.ArrowLeftIcon />
         </button>
-        <ModalBtn contents="basic" />
+        <ModalBtn>
+          <BasicModalContent />
+        </ModalBtn>
       </S.BasicHeaderLayout>
     </>
   );
