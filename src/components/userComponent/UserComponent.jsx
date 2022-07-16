3de41f17a4b5c65956a5ce7cab054d2f20687116
subincdev <subinc.dev@gmail.com>
2022-07-16 21:18:41 +0900

    chore: 버튼 컴포넌트화로 유저 컴포넌트 필요없는 코드 정리 및 팔로우 버튼 컴포넌트 가져오기

--- src/components/userComponent/UserComponent.jsx
+++ src/components/userComponent/UserComponent.jsx
@@ -3,14 +3,10 @@ import { useState } from 'react';
 import * as S from './userComponent.style';
 import ModalBtn from '../modal/ModalBtn/ModalBtn';
 import ProductModalContent from '../modal/modalContent/PostModalContent/PostModalContent';
+import FollowBtn from '../button/FollowBtn/FollowBtn';
+
 export default function UserComponent({ image, username, accountname }) {
   const path = window.location.href;
-  //추후에 팔로우 상태 서버에서 받아오기
-  const [isFollow, setIsFollow] = useState(false);
-  const handleFollowBtn = () => {
-    setIsFollow(!isFollow);
-  };
-
   return (
     <S.UserComponent>
       <S.ProfileImg src={image} alt="프로필이미지" />
@@ -20,11 +16,7 @@ export default function UserComponent({ image, username, accountname }) {
       </S.UserInfo>
       {/*페이지의 경로에 follow 있을 때 팔로우 버튼이 뜬다*/}
       {path.includes('follow') ? (
-        <S.FollowBtn
-          alt="팔로우 버튼"
-          isFollow={isFollow}
-          onClick={handleFollowBtn}
-        />
+        <FollowBtn size="small" />
       ) : (
         {
           /*페이지의 경로에 follow,search가 없을 때 모달 버튼이 뜬다*/
--- src/components/userComponent/userComponent.style.jsx
+++ src/components/userComponent/userComponent.style.jsx
@@ -20,33 +20,6 @@ export const ProfileImg = styled.img`
   border-radius: 50%;
 `;
 
-export const FollowBtn = styled.button`
-  &::after {
-    ${(props) =>
-      props.isFollow
-        ? css`
-            content: '취소';
-            color: ${props.theme.palette['mediumGray']};
-          `
-        : css`
-            content: '팔로우';
-            color: ${props.theme.palette['white']};
-          `};
-  }
-  ${(props) =>
-    props.isFollow
-      ? css`
-          background-color: ${props.theme.palette['white']};
-          border: 1px solid ${props.theme.palette['border']};
-        `
-      : css`
-          background-color: ${props.theme.palette['point']};
-          border: none;
-        `}
-  padding: 0.7em 1.2em;
-  border-radius: 2.6rem;
-`;
-
 export const UserInfo = styled.div`
   margin: 0.6em auto 0.7em 1.2em;
 `;
