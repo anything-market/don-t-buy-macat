c2d0b24966d30e6c59f5d7f5af180e08de2813ba
yunheejo <rhcjrdbsgml@gmail.com>
2022-07-19 01:06:56 +0900

    feat: 이미지파일 3개 업로드 제한
    useRef로 input과 imageUploadBtn 연결을 해주고 onClick이벤트를 주어
    이미지가 3개가 넘어가면 alert창을 띄우게 했습니다

--- src/components/postUpload/PostUpload.jsx
+++ src/components/postUpload/PostUpload.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import * as S from './postUpload.style';
+import { useRef, useState } from 'react';
 import UploadHeader from '../header/UploadHeader/UploadHeader';
 
 function PostUpload() {
+  const Upload_Input = useRef();
   const [text, setText] = useState(''); //입력텍스트
+  const [imageUrl, setImageUrl] = useState('');
 
   //입력창에 글을 쓰면 이벤트발생
   const handleChangeText = (e) => {
@@ -37,12 +40,19 @@ function PostUpload() {
             </S.UploadImg>
             <S.UploadInput
               className="A11yHidden"
-              // ref={Upload_Input}
+              ref={Upload_Input}
               type="file"
               accept="image/*"
               // onChange={handleChangeFile}
             />
-            <S.ImgUploadBtn />
+            <S.ImgUploadBtn
+              onClick={
+                () =>
+                  imageUrl.length === 3
+                    ? alert('이미지는 3개까지만 업로드할 수 있습니다.')
+                    : Upload_Input.current.click() //이 버튼을 클릭하면 file input을 클릭한겁니다.이벤트위임을 받았다
+              }
+            />
           </S.PostFormContainer>
         </S.PostForm>
       </S.PostUploadFieldSet>
