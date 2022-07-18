13e784ad1e1e7cf095558a4aa11337f3e004ceca
SiaKim <devkimsia@gmail.com>
2022-07-18 16:58:39 +0900

    feat: Search컴포넌트 내 onTyping 핸들러함수 추가 후 SearcHeader내의 변경사항을 받아오도록 변경

--- src/components/header/SearchHeader/SearchHeader.jsx
+++ src/components/header/SearchHeader/SearchHeader.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as S from './searchHeader.style';
 
-export default function SearchHeader() {
+export default function SearchHeader({ onTyping }) {
   const navigate = useNavigate();
   const handleGoPrev = () => {
     navigate(-1);
   };
+
   return (
     <S.BasicHeaderLayout>
       <button onClick={handleGoPrev}>
         <S.ArrowLeftIcon />
       </button>
-      <S.SearchInput />
+      <S.SearchInput
+        onChange={(e) => {
+          onTyping(e.target.value);
+        }}
+      />
     </S.BasicHeaderLayout>
   );
 }
