23070f9986ef50bc68ab0159d52db9c08b492535
subincdev <subinc.dev@gmail.com>
2022-07-19 20:16:36 +0900

    feat: 입양 고양이 이름 input 유효성 검사하는 로직 구현

--- src/components/adoptPost/AdoptPost.jsx
+++ src/components/adoptPost/AdoptPost.jsx
@@ -1,8 +1,19 @@
 import React, { useRef, useState } from 'react';
+import { useEffect } from 'react';
 import UploadHeader from '../header/UploadHeader/UploadHeader';
 import * as S from './adoptPost.style';
 export default function AdoptPost() {
+  const [itemName, setItemName] = useState('');
+  const [itemNameError, setItemNameError] = useState(false);
+  const [price, setPrice] = useState('');
+  const [priceError, setPriceError] = useState(false);
+  const [link, setLink] = useState('');
+  const [linkError, setLinkError] = useState(false);
   const [previewImg, setPreviewImg] = useState('');
+  const [previewImgError, setPreviewImgError] = useState(false);
+  //pass가 false일땐 버튼 disabled, true일땐 submit 가능
+  const pass = previewImgError && itemNameError && priceError && linkError;
+  console.log('데이터 전송 가능한가요?', pass);
   //파일 input
   const fileUploadBtn = useRef();
 
@@ -20,8 +31,31 @@ export default function AdoptPost() {
     reader.readAsDataURL(Blob);
     reader.onload = () => {
       setPreviewImg(reader.result);
+      setPreviewImgError(true);
     };
   };
+
+  //이름 유효성 검사
+  const handleItemName = (e) => {
+    if (e.target.value.length < 2) {
+      //2자보다 작을땐 input line red컬러로 바뀜
+      //error값은 false
+      e.target.style.borderBottomColor = 'red';
+      setItemNameError(false);
+    } else if (e.target.value.length > 15) {
+      //15자 이상 입력안되게 처리
+      return;
+    } else {
+      //유효한 값일 때
+      e.target.style.borderBottomColor = '#DBDBDB';
+      setItemNameError(true);
+    }
+    setItemName(e.target.value);
+  };
+  console.log('이미지 유효한가요?', previewImgError);
+  console.log('이름 유효한가요?', itemNameError);
+  //   console.log('책임비 유효한가요?', priceError);
+  //   console.log('링크 유효한가요?', linkError);
 
   return (
     <>
@@ -39,30 +73,30 @@ export default function AdoptPost() {
           accept="image/*"
           ref={fileUploadBtn}
           onChange={handleParseImg}
-          requried
         />
         <S.TxtLabel htmlFor="name">이름</S.TxtLabel>
         <S.TxtInput
           id="name"
           type="text"
           placeholder="2~15자 이내여야 합니다."
-          minLength="2"
-          maxLength="15"
-          requried
+          value={itemName}
+          onChange={handleItemName}
         />
         <S.TxtLabel htmlFor="price">책임비</S.TxtLabel>
         <S.TxtInput
           id="price"
           type="number"
           placeholder="숫자만 입력 가능합니다."
-          requried
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
         />
         <S.TxtLabel htmlFor="url">보호소 링크</S.TxtLabel>
         <S.TxtInput
           id="url"
           placeholder="URL을 입력해 주세요."
           type="text"
-          requried
+          value={link}
+          onChange={(e) => setLink(e.target.value)}
         />
       </S.Form>
     </>
