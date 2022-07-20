c0f6f5986dc0c140f622f3b946fc6d6a8b5df0fd
SiaKim <devkimsia@gmail.com>
2022-07-20 17:26:32 +0900

    feat: 팔로우목록 UI구현을 위하여 UserComponent내에 intro 프롭스 추가

--- src/components/userComponent/UserComponent.jsx
+++ src/components/userComponent/UserComponent.jsx
@@ -5,14 +5,16 @@ import ModalBtn from '../modal/ModalBtn/ModalBtn';
 import ProductModalContent from '../modal/modalContent/PostModalContent/PostModalContent';
 import FollowBtn from '../button/FollowBtn/FollowBtn';
 
-export default function UserComponent({ image, username, accountname }) {
+export default function UserComponent({ image, username, accountname, intro }) {
   const path = window.location.href;
   return (
     <S.UserComponent>
       <S.ProfileImg src={image} alt="프로필이미지" />
       <S.UserInfo>
         <S.UserName>{username}</S.UserName>
-        <S.UserIntro>{accountname}</S.UserIntro>
+        <S.UserIntro>
+          {path.includes('follow') ? intro : accountname}
+        </S.UserIntro>
       </S.UserInfo>
       {/*페이지의 경로에 follow 있을 때 팔로우 버튼이 뜬다, search,follow 페이지가 아닐경우 모달 버튼이 뜬다*/}
       {path.includes('follow') && <FollowBtn size="small" />}
