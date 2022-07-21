5ed0b14cbe2d988effd97ad334d489e97620933c
yunheejo <rhcjrdbsgml@gmail.com>
2022-07-21 09:20:28 +0900

    feat: 저장 버튼 활성화 기능 추가
    활성화 비활성화 상태를 PostUpload에서 props로 받아옵니다.

--- src/components/header/UploadHeader/UploadHeader.jsx
+++ src/components/header/UploadHeader/UploadHeader.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as S from './uploadHeader.style';
 import { BasicHeaderLayout } from '../commonHeader.style';
-export default function UploadHeader({ id }) {
+export default function UploadHeader({ id, isValid }) {
   const navigate = useNavigate();
   const handleGoPrev = () => {
     navigate(-1);
   };
+  console.log(isValid);
   return (
     <BasicHeaderLayout>
       <button onClick={handleGoPrev}>
         <S.ArrowLeftIcon />
       </button>
-      <button form={id}>
-        <S.SaveBtn />
-      </button>
+      <S.SaveBtn form={id} disabled={!isValid}>
+        저장
+      </S.SaveBtn>
     </BasicHeaderLayout>
   );
 }
