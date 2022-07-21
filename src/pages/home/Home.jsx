63cdc6abae58ca0c306cb98651c3e22e5226c7a5
SiaKim <devkimsia@gmail.com>
2022-07-21 10:05:57 +0900

    refactor: 직접 로그인하던 부분을 로컬스토리지에서 userToken 가져오도록 변경

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,66 +3,43 @@ import React, { useEffect, useState } from 'react';
 import PostCard from '../../components/post/PostCard/PostCard';
 import * as S from './Home.style';
 import { ReactComponent as Logo } from './../../assets/symbol-logo-feed.svg';
-import BasicHeader from '../../components/header/BasicHeader/BasicHeader';
+import MainHeader from './../../components/header/MainHeader/MainHeader';
 
 const Home = () => {
-  const [logInData, setlogInData] = useState();
+  const [userToken, setUserToken] = useState();
   const [userFeedData, setUserFeedData] = useState();
 
   // get user token when component first mounted
   useEffect(() => {
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-    const data = JSON.stringify({
-      user: { email: 'sisikhere@gmail.com', password: '123456' },
-    });
-    axios
-      .post('http://146.56.183.55:5050/user/login', data, config)
-      .then((response) => {
-        setlogInData(response.data.user);
-      })
-      .catch((error) => console.log(error));
+    const userToken = localStorage.getItem('Access Token');
+    setUserToken(userToken);
   }, []);
 
   // get feed data
-  const getFeedData = () => {
-    if (logInData) {
-      axios({
-        method: 'get',
-        url: 'http://146.56.183.55:5050/post/feed',
-        headers: {
-          Authorization: `Bearer ${logInData.token}`,
-          'Content-type': 'application/json',
-        },
-      }).then((response) => {
-        setUserFeedData(response.data.posts);
-      });
-    }
-  };
+  useEffect(() => {
+    const getFeedData = () => {
+      if (userToken) {
+        axios({
+          method: 'get',
+          url: 'http://146.56.183.55:5050/post/feed',
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+            'Content-type': 'application/json',
+          },
+        }).then((response) => {
+          setUserFeedData(response.data.posts);
+        });
+      }
+    };
+    getFeedData();
+  }, [userToken]);
 
   return (
     <>
       {userFeedData && userFeedData.length > 0 ? (
         <>
-          <BasicHeader />
+          <MainHeader />
           <S.PostWrap>
-            <button
-              style={{
-                backgroundColor: 'red',
-                color: 'white',
-                padding: '10px',
-                borderRadius: '10px',
-                marginBottom: '20px',
-              }}
-              onClick={() => {
-                setUserFeedData([]);
-              }}
-            >
-              피드나가기
-            </button>
             {userFeedData.map((post) => {
               return <PostCard key={post.id} data={post} />;
             })}
@@ -70,22 +47,12 @@ const Home = () => {
         </>
       ) : (
         <S.FeedEmptyWrap>
-          <button
-            style={{
-              backgroundColor: 'red',
-              color: 'white',
-              padding: '10px',
-              borderRadius: '10px',
-              marginBottom: '20px',
-            }}
-            onClick={getFeedData}
-          >
-            피드표시하기
-          </button>
           <div>
             <Logo />
             <S.FeedEmptyTxt>유저를 검색해 팔로우 해보세요!</S.FeedEmptyTxt>
-            <S.Button>검색하기</S.Button>
+            <a href="/search">
+              <S.Button src="/search">검색하기</S.Button>
+            </a>
           </div>
         </S.FeedEmptyWrap>
       )}
