d2824b1eb4671c2ba2dd7c119fb9c03af012e247
SiaKim <devkimsia@gmail.com>
2022-07-22 16:05:30 +0900

    feat: 사용자가 업로드한 이미지가 1장 이상인 경우  이미지 슬라이더로 표시되도록 함

--- src/components/post/PostImgContainer/PostImgContainer.jsx
+++ src/components/post/PostImgContainer/PostImgContainer.jsx
@@ -2,33 +2,68 @@ import React from 'react';
 import * as S from './PostImgContainer.style';
 
 const PostImgContainer = ({ image }) => {
-  console.log(image);
-  return (
-    <S.ImgContainer>
-      <S.ImgList>
-        {
-          //이미지가 있는 게시물이면서 단일이미지인 경우
-          image.length !== 0 && image.split(',').length <= 1 ? (
+  const multipleImages = image.split(',').length > 1;
+
+  const handleSliderBtn = (e) => {
+    const parentUl = e.target.parentNode.parentNode.childNodes[0];
+    const buttons = e.target.parentNode.childNodes;
+
+    //선택된 버튼에 인라인 색상 추가
+    for (let i = 0; i < buttons.length; i++) {
+      if (e.target.id === buttons[i].id) {
+        e.target.setAttribute('style', 'background-color:#05704A');
+      } else {
+        // buttons[i].classList.remove('current');
+        buttons[i].setAttribute('style', 'background-color:#fff');
+      }
+    }
+
+    //선택된 버튼의 id에 따라 표시되는 이미지 변경
+    if (e.target.id === 'slide-btn1') {
+      parentUl.setAttribute('style', 'transform:translate(0px)');
+    } else if (e.target.id === 'slide-btn2') {
+      parentUl.setAttribute('style', 'transform:translate(-304px)');
+    } else if (e.target.id === 'slide-btn3') {
+      parentUl.setAttribute('style', 'transform:translate(-608px)');
+    }
+  };
+
+  // 이미지가 있는 경우에만 렌더하기
+  if (image.length !== 0) {
+    return (
+      <S.ImgContainer>
+        <S.ImgList>
+          {multipleImages ? (
+            image.split(',').map((image) => {
+              return (
+                <S.ImgItem key={Math.floor(Math.random() * 1000)}>
+                  <S.PostImg src={image} alt="" />
+                </S.ImgItem>
+              );
+            })
+          ) : (
             <S.ImgItem>
               <S.PostImg src={image} alt="" />
             </S.ImgItem>
-          ) : null
-        }
-        {
-          //이미지가 있는 게시물이면서 복수이미지인 경우
-          image.length !== 0 && image.split(',').length > 1
-            ? image.split(',').map((image) => {
-                return (
-                  <S.ImgItem key={Math.random()}>
-                    <S.PostImg src={image} alt="" />
-                  </S.ImgItem>
-                );
-              })
-            : null
-        }
-      </S.ImgList>
-    </S.ImgContainer>
-  );
+          )}
+        </S.ImgList>
+        {multipleImages && (
+          <S.ImgSliderBtnWrap>
+            {image.split(',').map((item, index) => {
+              return (
+                <S.ImgSliderBtn
+                  key={Math.floor(Math.random() * 1000)}
+                  id={'slide-btn' + (index + 1)}
+                  onClick={handleSliderBtn}
+                  style={index === 0 ? { backgroundColor: '#05704A' } : null}
+                />
+              );
+            })}
+          </S.ImgSliderBtnWrap>
+        )}
+      </S.ImgContainer>
+    );
+  }
 };
 
 export default PostImgContainer;
--- src/components/post/PostImgContainer/PostImgContainer.style.jsx
+++ src/components/post/PostImgContainer/PostImgContainer.style.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const ImgList = styled.ul`
   display: flex;
+  /* box-sizing: border-box; */
   transition: all 0.4s;
 `;
 
@@ -26,4 +27,22 @@ export const ImgItem = styled.li`
 export const PostImg = styled.img`
   border-radius: 10px;
   margin-bottom: 16px;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export const ImgSliderBtnWrap = styled.div`
+  position: absolute;
+  display: flex;
+  gap: 6px;
+  left: 50%;
+  bottom: 16px;
+  transform: translateX(-50%);
+`;
+
+export const ImgSliderBtn = styled.button`
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+  background-color: #fff;
 `;
