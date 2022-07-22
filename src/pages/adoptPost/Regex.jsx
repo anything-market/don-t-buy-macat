18590a211919cdad55ccc30fcc5154dc4f226775
subincdev <subinc.dev@gmail.com>
2022-07-22 11:35:18 +0900

    chore: 입양 고양이 이름 숫자 허용으로 정규표현식 수정

--- src/pages/adoptPost/Regex.jsx
+++ src/pages/adoptPost/Regex.jsx
@@ -1,4 +1,4 @@
-const nameRegex = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{2,15}$/;
+const nameRegex = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|0-9]{2,15}$/;
 const priceRegex = /^[0-9\\,]*$/;
 const urlRegex =
   /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
