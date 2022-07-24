2c96a8581ed4fe8b223abd0142263deacbf1c4b6
Koreanhamster <jihyun.heo1226@gmail.com>
2022-07-24 17:55:22 +0900

    feat: 네비게이션바 기능구현 및 NavLink 연결/styling
    - NavLink 연결하여 클릭시 해당 페이지로 이동
    - 해당 페이지와 location pathname이 일치한다면 color이미지로 변경
    - 해당 페이지와 location pathname이 일치하지 않는다면 일반 이미지(흑백)으로 변경
    - NavLink className에 isActive를 파라미터로 받아 active상태라면 active 클래스 달아줌
    - active일 때 color point색깔로 스타일링

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import GlobalStyles from './styles/GlobalStyles';
 import { BrowserRouter } from 'react-router-dom';
 import Pages from './pages/Pages';
-import NavigationBar from './components/navigation-bar/NavigationBar';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <BrowserRouter>
         <GlobalStyles />
         <Pages />
-        <NavigationBar />
       </BrowserRouter>
     </div>
   );
--- src/components/navigation-bar/NavigationBar.jsx
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -1,55 +1,103 @@
 import React from 'react';
 import styled from 'styled-components';
-import tapHome from '../../assets/tab-icon-home.svg.svg';
+import tabHome from '../../assets/tab-icon-home.svg.svg';
 import tabMessage from '../../assets/tab-icon-message.svg.svg';
 import tabEdit from '../../assets/tab-icon-edit.svg.svg';
 import tabUser from '../../assets/tab-icon-user.svg.svg';
+import activeTabHome from '../../assets/tab-icon-home-color.svg.svg';
+import activeTabMessage from '../../assets/tab-icon-message-color.svg.svg';
+import activeTabEdit from '../../assets/tab-icon-edit-color.svg.svg';
+import activeTabUser from '../../assets/tab-icon-user-color.svg.svg';
+import { NavLink } from 'react-router-dom';
 
 const Wrapper = styled.div`
   margin-top: auto;
   width: 100%;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
 `;
 
-const FlexBox = styled.div`
+const NavBar = styled.ul`
   display: flex;
   justify-content: space-around;
   align-items: center;
 `;
 
-const FlexItem = styled.div`
+const StyleNavLink = styled(NavLink)`
+  color: ${(props) => props.theme.palette['mediumGray']};
+  font-weight: 400;
+  font-size: 1rem;
+
+  // NavLink가 active일 때 글씨색깔 변경
+  &.active {
+    color: ${(props) => props.theme.palette['point']};
+  }
+`;
+
+const List = styled.li`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 0.5rem;
+  cursor: pointer;
 
-  & > p {
-    color: ${(props) => props.theme.palette['mediumGray']};
-    font-weight: 400;
-    font-size: 1rem;
+  & > img {
+    width: 2rem;
   }
 `;
 
 function NavigationBar() {
   return (
     <Wrapper>
-      <FlexBox>
-        <FlexItem>
-          <img src={tapHome} />
-          <p>홈</p>
-        </FlexItem>
-        <FlexItem>
-          <img src={tabMessage} />
-          <p>홈</p>
-        </FlexItem>
-        <FlexItem>
-          <img src={tabEdit} />
-          <p>홈</p>
-        </FlexItem>
-        <FlexItem>
-          <img src={tabUser} />
-          <p>홈</p>
-        </FlexItem>
-      </FlexBox>
+      <NavBar>
+        <StyleNavLink
+          to={'/home'}
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
+          <List>
+            <img
+              src={location.pathname === '/home' ? activeTabHome : tabHome}
+            />
+            <p>홈</p>
+          </List>
+        </StyleNavLink>
+        <StyleNavLink
+          to={'/search'}
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
+          <List>
+            <img
+              src={
+                location.pathname === '/search' ? activeTabMessage : tabMessage
+              }
+            />
+            <p>채팅</p>
+          </List>
+        </StyleNavLink>
+        {/* post upload 페이지에서는 네비게이션 바 사라짐 */}
+        <StyleNavLink
+          to={'/upload'}
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
+          <List>
+            <img
+              src={location.pathname === '/upload' ? activeTabEdit : tabEdit}
+            />
+            <p>게시물 작성</p>
+          </List>
+        </StyleNavLink>
+        <StyleNavLink
+          to={'/adoptPost'}
+          className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+        >
+          <List>
+            <img
+              src={location.pathname === '/adoptPost' ? activeTabUser : tabUser}
+            />
+            <p>프로필</p>
+          </List>
+        </StyleNavLink>
+      </NavBar>
     </Wrapper>
   );
 }
