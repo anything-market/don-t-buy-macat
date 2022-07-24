f510662dcd216aafdd5298c01063aa3842af4289
subincdev <subinc.dev@gmail.com>
2022-07-24 20:40:37 +0900

    chore: 테스트 유저 데이터 파일로 분리

--- src/pages/chats/ChatList/ChatList.jsx
+++ src/pages/chats/ChatList/ChatList.jsx
@@ -1,42 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './chatList.style';
 import BasicHeader from '../../../components/header/BasicHeader/BasicHeader';
-import BasicProfile from '../../../assets/basic-profile.svg';
 import UserComponent from '../../../components/userComponent/UserComponent';
 import Chat from '../Chat/Chat';
 import { Link } from 'react-router-dom';
+import testuserInfo from '../TestUserInfo';
+
 export default function ChatList() {
-  const userInfo = [
-    {
-      id: 1,
-      image: BasicProfile,
-      username: '집사냥',
-      message: '보호소 방문 가능할까요?',
-      time: '2020.10.25',
-      messageTime: '12:39',
-    },
-    {
-      id: 2,
-      image: BasicProfile,
-      username: '얼룩이엄마',
-      message: '늘 감사합니다^^',
-      time: '2020.10.25',
-      messageTime: '12:39',
-    },
-    {
-      id: 3,
-      image: BasicProfile,
-      username: '천안시보호소',
-      message: '수고 많으십니다.',
-      time: '2020.10.25',
-      messageTime: '12:39',
-    },
-  ];
   return (
     <>
       <BasicHeader />
       <S.ChatListUl>
-        {userInfo.map((user) => (
+        {testuserInfo.map((user) => (
           <li key={user.id}>
             <Link to={`/chats/${user.id}`} element={<Chat />}>
               <UserComponent
@@ -45,6 +20,7 @@ export default function ChatList() {
                 username={user.username}
                 message={user.message}
                 time={user.time}
+                isRead={isRead}
               />
             </Link>
           </li>
