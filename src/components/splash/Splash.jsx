6f4cc2daa0e75353f8c37750181b41026ef220df
yunheejo <rhcjrdbsgml@gmail.com>
2022-07-25 15:05:24 +0900

    test: 컴포넌트 분리를 위해 테스트
    컴포넌트 분리를 위해 테스트합니다

--- src/components/splash/Splash.jsx
+++ src/components/splash/Splash.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import * as S from './splash.style';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
 
 function Splash() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    setTimeout(() => {
-      navigate('/home');
-    }, 2000);
-  }, []);
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     navigate('/home');
+  //   }, 2000);
+  // }, []);
 
   return (
     <>
