a2fb6c48caf6d496ece030ba18d6de3250910cb8
yunheejo <rhcjrdbsgml@gmail.com>
2022-07-25 15:06:43 +0900

    chore: 필요없는 주석제거

--- src/components/splash/Splash.jsx
+++ src/components/splash/Splash.jsx
@@ -1,16 +1,7 @@
 import React from 'react';
 import * as S from './splash.style';
-// import { useEffect } from 'react';
-// import { useNavigate } from 'react-router-dom';
 
 function Splash() {
-  // const navigate = useNavigate();
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     navigate('/home');
-  //   }, 2000);
-  // }, []);
-
   return (
     <>
       <S.SplashImgWrapper>
