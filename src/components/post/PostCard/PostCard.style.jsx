3663da7c8dcd8cf4356809dbb5e74b708a3810e0
SiaKim <devkimsia@gmail.com>
2022-07-25 11:42:38 +0900

    design: PostCard스타일 수정

--- src/components/post/PostCard/PostCard.style.jsx
+++ src/components/post/PostCard/PostCard.style.jsx
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
 
+export const PostCardWrap = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 export const ProfileWrap = styled.article`
-  margin-bottom: 20px;
+  margin: 2rem 0;
   position: relative;
   max-width: 358px;
   min-width: 358px;
