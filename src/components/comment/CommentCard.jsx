d0e2b03c0b489d13fa7a4c9421341ae4016ff53c
SiaKim <devkimsia@gmail.com>
2022-07-25 11:50:20 +0900

    feat: Post 내에 getPostData와 getComments 구현 후 PostCard 컴포넌트 import

--- src/components/comment/CommentCard.jsx
+++ src/components/comment/CommentCard.jsx
@@ -3,35 +3,16 @@ import React, { useState, useEffect } from 'react';
 import CommentList from './CommentList/CommentList';
 import CommentReply from './CommentReply/CommentReply';
 
-function CommentCard({ postId }) {
-  const [comments, setComments] = useState([]);
-  const [userToken, setUserToken] = useState('');
-  useEffect(() => {
-    const userToken = localStorage.getItem('Access Token');
-    setUserToken(userToken);
-  }, []);
+function CommentCard({ postId, comments, getComments }) {
+  // const [userToken, setUserToken] = useState('');
+  // useEffect(() => {
+  //   const userToken = localStorage.getItem('Access Token');
+  //   setUserToken(userToken);
+  // }, []);
 
-  const getComments = () => {
-    axios({
-      url: `http://146.56.183.55:5050/post/${postId}/comments?limit=10`,
-      method: 'get',
-      headers: {
-        Authorization: `Bearer ${userToken}`,
-        'Content-type': 'application/json',
-      },
-    })
-      .then((response) => {
-        console.log(response.data.comments);
-        setComments(response.data.comments);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  useEffect(() => {
-    getComments();
-  }, [userToken]);
+  // useEffect(() => {
+  //   getComments();
+  // }, [userToken]);
 
   return (
     <div className="CommentCard">
--- src/components/post/PostCard/PostCard.jsx
+++ src/components/post/PostCard/PostCard.jsx
@@ -1,29 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import * as S from './PostCard.style';
 import PostContent from '../PostContent/PostContent';
-import PostAuthor from './../PostAuthor/PostAuthor';
 import UserComponent from './../../userComponent/UserComponent';
 
-const PostCard = (data) => {
+const PostCard = ({ data }) => {
   const [authorData, setAuthorData] = useState();
   const [postData, setPostData] = useState();
 
   useEffect(() => {
-    setAuthorData(data.data.author);
-    setPostData(data.data);
+    if (data) {
+      setAuthorData(data.author);
+      setPostData(data);
+    }
   }, [data]);
 
   return (
-    <S.ProfileWrap>
-      {authorData && (
-        <UserComponent
-          image={authorData.image}
-          username={authorData.username}
-          accountname={authorData.accountname}
-        />
-      )}
-      {postData && <PostContent data={postData} />}
-    </S.ProfileWrap>
+    <S.PostCardWrap>
+      <S.ProfileWrap>
+        {authorData && (
+          <UserComponent
+            image={authorData.image}
+            username={authorData.username}
+            accountname={authorData.accountname}
+          />
+        )}
+        {postData && <PostContent data={postData} />}
+      </S.ProfileWrap>
+    </S.PostCardWrap>
   );
 };
 
