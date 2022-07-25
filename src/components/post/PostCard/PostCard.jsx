65252adac3a93334ce1c31666b927c0beeb3054d
SiaKim <devkimsia@gmail.com>
2022-07-25 13:26:05 +0900

    design: PostCard 가운데정렬 스타일 수정

--- src/components/post/PostCard/PostCard.jsx
+++ src/components/post/PostCard/PostCard.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from 'react';
 import * as S from './PostCard.style';
 import PostContent from '../PostContent/PostContent';
 import UserComponent from './../../userComponent/UserComponent';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const PostCard = ({ data }) => {
+  const navigate = useNavigate();
+
   const [authorData, setAuthorData] = useState();
   const [postData, setPostData] = useState();
 
@@ -16,16 +19,18 @@ const PostCard = ({ data }) => {
 
   return (
     <S.PostCardWrap>
-      <S.ProfileWrap>
-        {authorData && (
-          <UserComponent
-            image={authorData.image}
-            username={authorData.username}
-            accountname={authorData.accountname}
-          />
-        )}
-        {postData && <PostContent data={postData} />}
-      </S.ProfileWrap>
+      <S.PostCardContainer>
+        <S.ProfileWrap>
+          {authorData && (
+            <UserComponent
+              image={authorData.image}
+              username={authorData.username}
+              accountname={authorData.accountname}
+            />
+          )}
+          {postData && <PostContent data={postData} />}
+        </S.ProfileWrap>
+      </S.PostCardContainer>
     </S.PostCardWrap>
   );
 };
--- src/components/post/PostCard/PostCard.style.jsx
+++ src/components/post/PostCard/PostCard.style.jsx
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 
 export const PostCardWrap = styled.div`
   width: 100%;
+  display: block;
+`;
+
+export const PostCardContainer = styled.div`
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
