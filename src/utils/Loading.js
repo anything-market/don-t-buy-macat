a867abf3f02f09e6274e54951424949331b38837
SiaKim <devkimsia@gmail.com>
2022-07-26 18:52:46 +0900

    remove: spinner라이브러리 삭제

--- src/utils/Loading.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Oval } from 'react-loader-spinner';
-
-function Loading() {
-  return <Oval color="#FADF53" height={50} width={50} />;
-}
-
-export default Loading;
