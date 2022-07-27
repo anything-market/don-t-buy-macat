a6e7153fec6bb266bce3bce4ccdb97850d028c36
subincdev <subinc.dev@gmail.com>
2022-07-27 17:27:44 +0900

    feat: 넘겨받은 postId를 통해 게시물 삭제 후 해당 페이지 리로드하도록 로직 구현

--- src/components/modal/alert/PostDeleteAlert/PostDeleteAlert.jsx
+++ src/components/modal/alert/PostDeleteAlert/PostDeleteAlert.jsx
@@ -1,19 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import * as S from '../commonAlert.style';
-export default function PostDeleteAlert({ handleCloseAlert }) {
+export default function PostDeleteAlert({ handleCloseAlert, postId }) {
   const [userToken, setUserToken] = useState('');
-
   useEffect(() => {
     const userToken = localStorage.getItem('Access Token');
     setUserToken(userToken);
   }, []);
+
+  //입양 등록 게시물 삭제
+  const handlePostDelete = async () => {
+    try {
+      const res = await axios
+        .delete(`https://mandarin.api.weniv.co.kr/product/${postId}`, {
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+            'Content-type': 'application/json',
+          },
+        })
+        .then(() => {
+          location.reload();
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <S.AlertContainer>
       <S.AlertMessage>해당 게시물을 삭제할까요?</S.AlertMessage>
       <S.AlertBtnContainer>
         <S.AlertBtn onClick={handleCloseAlert}>취소</S.AlertBtn>
-        <S.AlertBtn>삭제</S.AlertBtn>
+        <S.AlertBtn onClick={handlePostDelete}>삭제</S.AlertBtn>
       </S.AlertBtnContainer>
     </S.AlertContainer>
   );
