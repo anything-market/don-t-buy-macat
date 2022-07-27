79bbaaec66d8a03ab1a84b4ec1aeeab025a9c42d
subincdev <subinc.dev@gmail.com>
2022-07-27 17:09:39 +0900

    feat: delete를 하기 위해 유저 토큰 localStorage에서 받아오기

--- src/components/modal/alert/PostDeleteAlert/PostDeleteAlert.jsx
+++ src/components/modal/alert/PostDeleteAlert/PostDeleteAlert.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from '../commonAlert.style';
 export default function PostDeleteAlert({ handleCloseAlert }) {
+  const [userToken, setUserToken] = useState('');
+
+  useEffect(() => {
+    const userToken = localStorage.getItem('Access Token');
+    setUserToken(userToken);
+  }, []);
+
   return (
     <S.AlertContainer>
       <S.AlertMessage>해당 게시물을 삭제할까요?</S.AlertMessage>
