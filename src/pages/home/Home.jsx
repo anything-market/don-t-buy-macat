58065166605443d8800780bcedd5979f2370181e
SiaKim <devkimsia@gmail.com>
2022-07-27 14:02:21 +0900

    chore: feed가 비어있을때 MainHeader 추가

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -53,15 +53,18 @@ const Home = () => {
           </S.PostWrap>
         </>
       ) : (
-        <S.FeedEmptyWrap>
-          <div>
-            <Logo />
-            <S.FeedEmptyTxt>유저를 검색해 팔로우 해보세요!</S.FeedEmptyTxt>
-            <a href="/search">
-              <S.Button src="/search">검색하기</S.Button>
-            </a>
-          </div>
-        </S.FeedEmptyWrap>
+        <>
+          <MainHeader />
+          <S.FeedEmptyWrap>
+            <div>
+              <Logo />
+              <S.FeedEmptyTxt>유저를 검색해 팔로우 해보세요!</S.FeedEmptyTxt>
+              <a href="/search">
+                <S.Button src="/search">검색하기</S.Button>
+              </a>
+            </div>
+          </S.FeedEmptyWrap>
+        </>
       )}
       <NavigationBar />
     </>
