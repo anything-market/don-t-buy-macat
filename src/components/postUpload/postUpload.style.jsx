f8f7fe56a8f086b22f9930002264fb79e510609a
yunheejo <rhcjrdbsgml@gmail.com>
2022-07-29 17:47:23 +0900

    design: 이미지 삭제 버튼 스타일컴포넌트 추가
    이미지 삭제버튼 스타일컴포넌트 추가했습니다

--- src/components/postUpload/postUpload.style.jsx
+++ src/components/postUpload/postUpload.style.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import uploadIcon from '../../assets/upload-file.svg';
 import basicProfile from '../../assets/basic-profile.svg';
+import ImageDelete from '../../assets/x.svg';
 
 export const PostUploadWrapper = styled.form`
   width: 100%;
@@ -71,9 +72,24 @@ export const ImgUploadBtn = styled.img.attrs({
   cursor: pointer;
 `;
 
+export const Item = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
 export const PreviewImage = styled.img`
+  position: relative;
   margin: 10px 0 0 15px;
   width: 200px;
   height: auto;
   border-radius: 10px;
+`;
+
+export const ImageDeleteBtn = styled.button`
+  position: absolute;
+  top: 1.8rem;
+  right: 0.6rem;
+  width: 2.2rem;
+  height: 2.2rem;
+  background: url(${ImageDelete}) no-repeat center / contain;
 `;
