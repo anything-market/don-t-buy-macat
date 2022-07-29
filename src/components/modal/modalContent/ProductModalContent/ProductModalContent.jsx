8f4f1e24ff0a768b6eb7658bd37095f39776c707
SiaKim <devkimsia@gmail.com>
2022-07-29 22:35:17 +0900

    Revert "Revert "Merge branch 'develop' of https://github.com/anything-market/don-t-buy-macat into feat/userProfile""
    This reverts commit 21f4856143601b1c4800e9ba6a561914e266b83c.
    
    리버트#

--- src/components/modal/modalContent/ProductModalContent/ProductModalContent.jsx
+++ src/components/modal/modalContent/ProductModalContent/ProductModalContent.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 // import ProductDeleteAlert from '../HeaderModal/LogoutAlert/LogoutAlert';
 import PostDeleteAlert from '../../alert/PostDeleteAlert/PostDeleteAlert';
-export default function ProductModalContent() {
+export default function ProductModalContent({ postId }) {
   //alert창 열기
   const [isOpenAlert, setIsOpenAlert] = useState(false);
   const handleOpenAlert = () => {
@@ -21,7 +21,9 @@ export default function ProductModalContent() {
       <li>
         <button>보호소 웹사이트 방문하기</button>
       </li>
-      {isOpenAlert && <PostDeleteAlert handleCloseAlert={handleCloseAlert} />}
+      {isOpenAlert && (
+        <PostDeleteAlert handleCloseAlert={handleCloseAlert} postId={postId} />
+      )}
     </>
   );
 }
